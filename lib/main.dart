08cc93d9ce803d0dc2129b492ad67ebf54abc9c6
Mohitchaudhary0015 <Chaudharymohit0120@gmail.com>
2025-07-30 23:35:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
-
 import 'HomeScreen.dart';
 
-
 void main() {
-  runApp(const MaterialApp(home:HomeScreen()));
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false, 
+    );
+  }
+}
